ee70892dc664b9033675d95dca17c9d1b274d240
Loyio <i@loyio.me>
2020-11-12 15:15:09 +0800

    initial project

---
+++ src/main/java/com/texinc/loytesting/hrsystemplus/HrsystemplusApplication.java
@@ -0,0 +1,13 @@
+package com.texinc.loytesting.hrsystemplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrsystemplusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HrsystemplusApplication.class, args);
+    }
+
+}
