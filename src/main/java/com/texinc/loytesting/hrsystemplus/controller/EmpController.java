8d8cf74eae2e44abb0266eb2d469ad052028df00
Loyio <i@loyio.me>
2020-11-17 13:43:42 +0800

    add some class,and update service

--- src/main/java/com/texinc/loytesting/hrsystemplus/controller/EmpController.java
+++ src/main/java/com/texinc/loytesting/hrsystemplus/controller/EmpController.java
@@ -1,19 +1,15 @@
 package com.texinc.loytesting.hrsystemplus.controller;
 
-import com.texinc.loytesting.hrsystemplus.bean.Employee;
-import com.texinc.loytesting.hrsystemplus.bean.Position;
-import com.texinc.loytesting.hrsystemplus.bean.RespBean;
+import com.texinc.loytesting.hrsystemplus.bean.*;
 import com.texinc.loytesting.hrsystemplus.common.poi.PoiUtils;
-import com.texinc.loytesting.hrsystemplus.service.DepartmentService;
-import com.texinc.loytesting.hrsystemplus.service.EmpService;
-import com.texinc.loytesting.hrsystemplus.service.JobLevelService;
-import com.texinc.loytesting.hrsystemplus.service.PositionService;
+import com.texinc.loytesting.hrsystemplus.service.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.thymeleaf.TemplateEngine;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -26,97 +22,91 @@ import java.util.Map;
 @RequestMapping("/employee/basic")
 public class EmpController {
     @Autowired
-    EmpService empService;
+    EmpService employeeService;
     @Autowired
-    DepartmentService departmentService;
+    NationService nationService;
+    @Autowired
+    PoliticsStatusService politicsstatusService;
+    @Autowired
+    JobLevelService jobLevelService;
     @Autowired
     PositionService positionService;
     @Autowired
-    JobLevelService jobLevelService;
+    DepartmentService departmentService;
 
-    @Autowired
-    TemplateEngine templateEngine;
-
-    @RequestMapping(value = "/basicdata", method = RequestMethod.GET)
-    public Map<String, Object> getAllNations() {
-        Map<String, Object> map = new HashMap<>();
-        map.put("nations", empService.getAllNations());
-        map.put("politics", empService.getAllPolitics());
-        map.put("deps", departmentService.getDepByPid(-1L));
-        map.put("positions", positionService.getAllPos());
-        map.put("joblevels", jobLevelService.getAllJobLevels());
-        map.put("workID", String.format("%08d", empService.getMaxWorkID() + 1));
-        return map;
+    @GetMapping("/")
+    public RespPageBean getEmployeeByPage(@RequestParam(defaultValue = "1") Integer page, @RequestParam(defaultValue = "10") Integer size, Employee employee, Date[] beginDateScope) {
+        return employeeService.getEmployeeByPage(page, size, employee,beginDateScope);
     }
 
-    @RequestMapping("/maxWorkID")
-    public String maxWorkID() {
-        return String.format("%08d", empService.getMaxWorkID() + 1);
-    }
-
-    @RequestMapping(value = "/emp", method = RequestMethod.POST)
-    public RespBean addEmp(Employee employee) {
-        if (empService.addEmp(employee) == 1) {
-            List<Position> allPos = positionService.getAllPos();
-            for (Position allPo : allPos) {
-                if (allPo.getId() == employee.getPosId()) {
-                    employee.setPosName(allPo.getName());
-                }
-            }
+    @PostMapping("/")
+    public RespBean addEmp(@RequestBody Employee employee) {
+        if (employeeService.addEmp(employee) == 1) {
             return RespBean.ok("添加成功!");
         }
         return RespBean.error("添加失败!");
     }
 
-    @RequestMapping(value = "/emp", method = RequestMethod.PUT)
-    public RespBean updateEmp(Employee employee) {
-        if (empService.updateEmp(employee) == 1) {
-            return RespBean.ok("更新成功!");
-        }
-        return RespBean.error("更新失败!");
-    }
-
-    @RequestMapping(value = "/emp/{ids}", method = RequestMethod.DELETE)
-    public RespBean deleteEmpById(@PathVariable String ids) {
-        if (empService.deleteEmpById(ids)) {
+    @DeleteMapping("/{id}")
+    public RespBean deleteEmpByEid(@PathVariable Integer id) {
+        if (employeeService.deleteEmpByEid(id) == 1) {
             return RespBean.ok("删除成功!");
         }
         return RespBean.error("删除失败!");
     }
 
-    @RequestMapping(value = "/emp", method = RequestMethod.GET)
-    public Map<String, Object> getEmployeeByPage(
-            @RequestParam(defaultValue = "1") Integer page,
-            @RequestParam(defaultValue = "10") Integer size,
-            @RequestParam(defaultValue = "") String keywords,
-            Long politicId, Long nationId, Long posId,
-            Long jobLevelId, String engageForm,
-            Long departmentId, String beginDateScope) {
-        Map<String, Object> map = new HashMap<>();
-        List<Employee> employeeByPage = empService.getEmployeeByPage(page, size,
-                keywords,politicId, nationId, posId, jobLevelId, engageForm,
-                departmentId, beginDateScope);
-        Long count = empService.getCountByKeywords(keywords, politicId, nationId,
-                posId,jobLevelId, engageForm, departmentId, beginDateScope);
-        map.put("emps", employeeByPage);
-        map.put("count", count);
-        return map;
-    }
-
-    @RequestMapping(value = "/exportEmp", method = RequestMethod.GET)
-    public ResponseEntity<byte[]> exportEmp() {
-        return PoiUtils.exportEmp2Excel(empService.getAllEmployees());
-    }
-
-    @RequestMapping(value = "/importEmp", method = RequestMethod.POST)
-    public RespBean importEmp(MultipartFile file) {
-        List<Employee> emps = PoiUtils.importEmp2List(file,
-                empService.getAllNations(), empService.getAllPolitics(),
-                departmentService.getAllDeps(), positionService.getAllPos(),
-                jobLevelService.getAllJobLevels());
-        if (empService.addEmps(emps) == emps.size()) {
-            return RespBean.ok("导入成功!");
+    @PutMapping("/")
+    public RespBean updateEmp(@RequestBody Employee employee) {
+        if (employeeService.updateEmp(employee) == 1) {
+            return RespBean.ok("更新成功!");
         }
-        return RespBean.error("导入失败!");
+        return RespBean.error("更新失败!");
+    }
+
+    @GetMapping("/nations")
+    public List<Nation> getAllNations() {
+        return nationService.getAllNations();
+    }
+
+    @GetMapping("/politicsstatus")
+    public List<PoliticsStatus> getAllPoliticsstatus() {
+        return politicsstatusService.getAllPoliticsstatus();
+    }
+
+    @GetMapping("/joblevels")
+    public List<JobLevel> getAllJobLevels() {
+        return jobLevelService.getAllJobLevels();
+    }
+
+    @GetMapping("/positions")
+    public List<Position> getAllPositions() {
+        return positionService.getAllPositions();
+    }
+
+    @GetMapping("/maxWorkID")
+    public RespBean maxWorkID() {
+        RespBean respBean = RespBean.build().setStatus(200)
+                .setObj(String.format("%08d", employeeService.maxWorkID() + 1));
+        return respBean;
+    }
+
+    @GetMapping("/deps")
+    public List<Department> getAllDepartments() {
+        return departmentService.getAllDepartments();
+    }
+
+    @GetMapping("/export")
+    public ResponseEntity<byte[]> exportData() {
+        List<Employee> list = (List<Employee>) employeeService.getAllEmployees();
+        return PoiUtils.exportEmp2Excel(list);
+    }
+
+    @PostMapping("/import")
+    public RespBean importData(MultipartFile file) throws IOException {
+        List<Employee> list = PoiUtils.exportEmp2Excel(file, nationService.getAllNations(), politicsstatusService.getAllPoliticsstatus(), departmentService.getAllDepartmentsWithOutChildren(), positionService.getAllPositions(), jobLevelService.getAllJobLevels());
+        if (employeeService.addEmps(list) == list.size()) {
+            return RespBean.ok("上传成功");
+        }
+        return RespBean.error("上传失败");
     }
 }
